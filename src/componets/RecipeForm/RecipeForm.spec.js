a19c8fe23e63b46bd883053eee9ac6baa5fb91fa
Quan Xu <xuquan69220@gmail.com>
2022-06-30 13:45:02 +0200

    Add more testing

--- src/componets/RecipeForm/RecipeForm.spec.js
+++ src/componets/RecipeForm/RecipeForm.spec.js
@@ -1,5 +1,5 @@
 import {render, screen} from '@testing-library/react';
-//import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 
 import RecipeForm from './RecipeForm.js';
 
@@ -9,5 +9,36 @@ describe('RecipeForm', () => {
     screen.getByRole('form');
     screen.getByLabelText('Add Recipe:');
     screen.getByRole('button', {name: 'Add new recipe'});
+  });
+
+  it('calls onCreateRecipe on submit', async () => {
+    const user = userEvent.setup();
+    const callback = jest.fn();
+    render(<RecipeForm onCreateRecipe={callback} />);
+
+    const input = screen.getByLabelText('Add Recipe:');
+    await user.type(input, 'Bibimbap{Enter}');
+    expect(callback).toHaveBeenCalledWith('Bibimbap');
+  });
+
+  it('does trim whitespace before submit', async () => {
+    const user = userEvent.setup();
+    const callback = jest.fn();
+    render(<RecipeForm onCreateRecipe={callback} />);
+
+    const input = screen.getByLabelText('Add Recipe:');
+    await user.type(input, '      Thai curry soup    {Enter}');
+    expect(callback).toHaveBeenCalledWith('Thai curry soup');
+  });
+
+  it('does not call onCreateRecipe with only whitespace', async () => {
+    const user = userEvent.setup();
+    const callback = jest.fn();
+
+    render(<RecipeForm onCreateRecipe={callback} />);
+
+    const input = screen.getByLabelText('Add Recipe:');
+    await user.type(input, '           {Enter}');
+    expect(callback).not.toHaveBeenCalled();
   });
 });
