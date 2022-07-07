94431c25093eaa91eac6be00c66a37d3b9ce6386
Quan Xu <xuquan69220@gmail.com>
2022-07-07 10:50:43 +0200

    fix vercel issue retry

--- src/components/RecipeForm/RecipeForm.spec.js
+++ src/components/RecipeForm/RecipeForm.spec.js
@@ -1,7 +1,7 @@
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import RecipeForm from './RecipeForm.js.js';
+import RecipeForm from './RecipeForm.js';
 
 describe('RecipeForm', () => {
   it('is an accessible form', () => {
