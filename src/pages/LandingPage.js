bf0dac9f978d83b086b8f8b2764a560ccd5ec63f
Quan Xu <xuquan69220@gmail.com>
2022-07-20 15:31:15 +0200

    Add more styling for landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,36 +1,45 @@
 import {BsFillArrowRightCircleFill} from 'react-icons/bs';
+import {useNavigate} from 'react-router-dom';
 import styled from 'styled-components';
 
-// import BackgroundImage from '../image/background.png';
+//import BackgroundImage from '../image/background.png';
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate(`/recipes`);
+  };
   return (
-    <PageContainer>
-      {/* <TextContainer> */}
+    <>
       <AppTitle>Your digital recipe book</AppTitle>
-      <ButtonStart>
+      <ButtonStart type="button" onClick={handleRedirect}>
         <span>Start cooking</span> <BsFillArrowRightCircleFill id="icon-start" />
       </ButtonStart>
-      {/* </TextContainer> */}
-      {/* 
-      <ImageContainer>
+
+      {/* <ImageContainer>
         <Image src={BackgroundImage} alt="food" />
       </ImageContainer> */}
-    </PageContainer>
+    </>
   );
 }
 
-const PageContainer = styled.div`
-  margin-top: 5rem;
-  background-image: url('https://images.unsplash.com/photo-1543362906-acfc16c67564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JlZW4lMjB2ZWdldGFibGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60');
-
+// const PageContainer = styled.div`
+//   margin-top: 5rem;
+//   background-color: yellow;
+//   border: 2px solid black;
+//   position: relative;
+/* background-image: url('https://images.unsplash.com/photo-1543362906-acfc16c67564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JlZW4lMjB2ZWdldGFibGVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60'); */
+/* 
   width: 100%;
-  height: 100vh;
-`;
+  height: 100vh; */
 
 const AppTitle = styled.h1`
-  font-size: 3rem;
+  text-align: center;
+  margin-top: 10rem;
+  font-size: 3.8rem;
   color: var(--primary-color);
+  /* border: 2px solid red; */
 `;
 
 // const ImageContainer = styled.div`
@@ -55,8 +64,9 @@ const AppTitle = styled.h1`
 // `;
 
 const ButtonStart = styled.button`
+  /* border: 1px solid hotpink; */
   border: none;
-  border-radius: 6px;
+  border-radius: 8px;
   font-size: 1.5rem;
   background-color: var(--secondary-color);
   color: var(--white);
@@ -64,6 +74,10 @@ const ButtonStart = styled.button`
   /* margin: 1.5rem 0 0 9rem; */
   display: flex;
   align-items: center;
+  cursor: pointer;
+  position: absolute;
+  bottom: 30%;
+  right: 2.5rem;
 
   #icon-start {
     margin-left: 0.5rem;
